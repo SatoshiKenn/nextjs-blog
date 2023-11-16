80698dc479427ab561a488aa45c936509d944fe4
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2023-11-16 02:32:57 -0300

    blog app update

--- nextjs-blog/components/date.js
+++ nextjs-blog/components/date.js
@@ -1,21 +1,6 @@
-/*import { parseISO, format } from "date-fns";
+import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
-}
-*/
-
-import { parseISO, isValid, format } from 'date-fns';
-
-export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-
-  // Check if the date is valid before proceeding
-  if (!isValid(date)) {
-    console.error('Invalid date string:', dateString);
-    return null; // or handle it in another way
-  }
-
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
